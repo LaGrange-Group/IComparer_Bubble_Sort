30053f8fe57edafebb40d12230b549922d51327f
Lagrango <davidnlagrange@hotmail.com>
2018-11-09 08:47:03 -0600

    Set up Unit Test + Added Icomparer Interface + Created Empty Sort Method

--- IComparer_Bubble_Sort/SortList.cs
+++ IComparer_Bubble_Sort/SortList.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,25 @@ using System.Threading.Tasks;
 
 namespace IComparer_Bubble_Sort
 {
-    class SortList
+    public class SortList : IComparer
     {
+        public List<int> intList;
+        public SortList()
+        {
+            intList = new List<int>() { 5, 2, 3, 1, 4 };
+        }
+
+        public int Compare(object x, object y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public dynamic Sort()
+        {
+
+            List<int> listSorted = new List<int>();
+
+            return listSorted;
+        }
     }
 }
---
+++ UnitTesting_SortList/SortListTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IComparer_Bubble_Sort;
+using System.Collections.Generic;
+
+namespace UnitTesting_SortList
+{
+    [TestClass]
+    public class SortListTest
+    {
+        [TestMethod]
+        public void SortList_Test_Int()
+        {
+            // Arrange
+            SortList sortList = new SortList();
+            List<int> expected;
+            List<int> actual;
+
+            // Act
+            sortList.intList = new List<int>() {5, 2, 3, 1, 4 };
+            expected = new List<int>() {1, 2, 3, 4, 5 };
+
+            //This sort uses intList to sort for this test project!
+            actual = sortList.Sort();
+
+            // Assert
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+            Assert.AreEqual(expected[4], actual[4]);
+        }
+    }
+}
